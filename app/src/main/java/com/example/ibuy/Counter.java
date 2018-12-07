3929ddacb664365601fcf8bc214d099a04d88f24
Ariful Islam <ariful6@yahoo.com>
2018-12-07 04:23:27 +0200

    counter

---
+++ app/src/main/java/com/example/ibuy/Counter.java
@@ -0,0 +1,32 @@
+package com.example.ibuy;
+
+
+public class Counter {
+    private int counter;
+
+    public Counter() {
+        this.counter = counter;
+    }
+
+    public void increment() {
+        if (this.counter <= 10) {
+            this.counter++;
+
+        }
+    }
+
+    public void decrement() {
+        if (this.counter > 0) {
+            this.counter--;
+        }
+    }
+
+    public void reset() {
+        this.counter = 0;
+    }
+
+    public int getcounter() {
+        return this.counter;
+    }
+}
+
